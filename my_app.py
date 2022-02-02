80ccaf6329d75cb9470e41dc2bf8a957d5ef51a0
Yaroslaw60 <yarosl.markov2010@yandex.ru>
2022-02-03 02:09:12 +0300

    Добавил функции по распаковке JSON

--- my_app.py
+++ my_app.py
@@ -1,9 +1,24 @@
 import json
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
-
-with open("candidates.json", "rt", encoding="utf-8") as file:
-    data = json.load(file)
+app = Flask (__name__)
 
 
+def open_candidates_file():
+    with open("candidates.json", "rt", encoding="utf-8") as fp:
+        data = json.load(fp)
+        return data
+
+def open_settings_file():
+    with open("settings.json", "rt", encoding="utf-8") as file:
+        settings = json.load(file)
+        return settings
+
+
+@app.route("/")
+def main_page():
+    return "Hello world!"
+
+
+if __name__ == "__main__":
+    app.run()
